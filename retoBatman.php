fdd74d843ad9401d374a115cdb06228f21ea699d
GustavoSierra <gsierra.business@gmail.com>
2021-11-29 11:13:37 +0100

    retoBatman hecho

---
+++ retoBatman.php
@@ -0,0 +1,23 @@
+<?php 
+//Array de 3 platos
+$array =[
+    [
+        'titulo' => 'hamburguesa',
+        'comensales' => 1,
+        'plato' => 'primero',
+        'ingredientes' => ['pan', 'carne', 'queso', 'bacon']
+    ],
+    [
+        'titulo' => 'pizza',
+        'comensales' => 2,
+        'plato' => 'segundo',
+        'ingredientes' =>  ['masa', 'mozzarella', 'salsa de tomate', 'albahaca']
+    ],
+    [
+        'titulo' => 'alitas de pollo',
+        'comensales' => 2,
+        'plato' => 'entrante',
+        'ingredientes' => $array = ['pollo', 'salsa']
+    ]
+]   
+?>
